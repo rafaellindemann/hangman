55e57713df855eba554f7b1c401dee3d27d8bc4b
rafaellindemann <rafaelld@gmail.com>
2026-04-06 15:43:53 -0300

    att endereços relativos

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,5 @@ import Hangman from './components/Hangman';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
     <Hangman />
-  </React.StrictMode>
 );
